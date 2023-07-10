71237eac0bbc9bbf7816e1da2acdbd86200f5596
Rishi-dubey <117086580+Rishi-dubey@users.noreply.github.com>
2023-07-10 15:06:43 +0100

    Delete Clothes directory

--- Clothes/src/main/java/dev/Ecommerce/Clothes/repository/ConfirmRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.Ecommerce.Clothes.repository;
-
-import dev.Ecommerce.Clothes.models.Confirm;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ConfirmRepository extends JpaRepository<Confirm,Integer> {
-}
