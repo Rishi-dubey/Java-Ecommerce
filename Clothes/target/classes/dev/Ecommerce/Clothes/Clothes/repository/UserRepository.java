b3e5209672dc4bc82b690aaa873c3615d6a8dedb
Rishi-dubey <117086580+Rishi-dubey@users.noreply.github.com>
2023-07-10 15:04:46 +0100

    Delete Clothes/target/classes/dev/Ecommerce/Clothes directory

--- Clothes/target/classes/dev/Ecommerce/Clothes/Clothes/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.Ecommerce.Clothes.repository;
-
-import dev.Ecommerce.Clothes.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-
-    User findByUsernameAndPassword(String username, String pwd);
-}
